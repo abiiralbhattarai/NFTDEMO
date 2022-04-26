eab4597210c2ed4f6256e565bd6ea81fdf19fd46
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-04-26 18:17:51 +0545

    Smart Contract written

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -6,4 +6,23 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract MyNft is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
 
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("AbiNft", "ABN") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
